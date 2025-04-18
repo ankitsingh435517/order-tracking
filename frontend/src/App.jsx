4884b60d53e29cc5f4b4dfca178cff1052fbb0e2
Ankit Singh <ankitsingh435517@gmail.com>
2025-04-18 17:53:12 +0530

    add hashrouter for vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Agent from "./pages/Agent";
 import Customer from "./pages/Customer";
 
